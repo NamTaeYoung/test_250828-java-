bf5f9a0a51a57894662c58b35dca89ff2a8434c4
NamTaeYoung <knnn47@naver.com>
2025-08-28 10:31:20 +0900

    교과목 평가(java)

---
+++ ArrayList.java
@@ -0,0 +1,27 @@
+package test;
+
+import java.util.Random;
+
+public class ArrayList {
+	public static void main(String[] args) {
+		Random r = new Random();
+		int[][]  array= new int[3][4];
+		int sum=0;
+		int count = 0;
+		
+		for (int i = 0; i < array.length; i++) {
+			for (int j = 0; j < array[i].length; j++) {
+				array[i][j]=r.nextInt(10)+1;
+				array[i][j]=r.nextInt(10)+1;
+				int a = array[i][j];
+				sum +=a;
+				count++;
+				System.out.print(array[i][j]+"\t");
+				if(count %4 == 0) {
+					System.out.println();
+				}
+			}
+		}
+		System.out.println("합은 "+sum);
+	}
+}
